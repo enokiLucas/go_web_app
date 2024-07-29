d193b3d945c67ed0697f5451bc51820f7b4e91e9
Lucas Enoki <enoki.lucas@gmail.com>
2024-07-29 16:07:44 -0300

    Update the game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,9 @@
 class Game < ApplicationRecord
+  validates :state, presence: true
+  validates :board_size, presence: true
+  validates :status, presence: true
+
+  belongs_to :player_black_id, class_name: "User", optional: true
+  belongs_to :player_white_id, class_name: "User", optional: true
+
 end
