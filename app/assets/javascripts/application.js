58b91860eb0490dd8c442fdd87115b42aaf68b98
Lucas Enoki <enoki.lucas@gmail.com>
2024-07-29 08:09:08 -0300

    Remove buttons ustom elements from the application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,7 +4,5 @@
 document.addEventListener('turbo:load', () => {
   // Initialize custom elements
   customElements.define('go-board', GoBoard);
-  customElements.define('pass-button', PassButton);
-  customElements.define('resign-button', ResignButton);
   // ... initialize other custom elements
 });
