62789141759ab574a3e89bb3fe3f92af9c734399
Lucas Enoki <enoki.lucas@gmail.com>
2024-07-29 15:43:43 -0300

    modify the migration

--- db/migrate/20240729183321_create_games.rb
+++ db/migrate/20240729183321_create_games.rb
@@ -1,8 +1,8 @@
 class CreateGames < ActiveRecord::Migration[7.1]
   def change
     create_table :games do |t|
-      t.json :state
-      t.integer :board_size
+      t.json :state, null: false
+      t.integer :board_size, null: false, default: 9
       t.string :game_type
       t.string :timer
       t.integer :player_black_id
@@ -10,6 +10,9 @@ class CreateGames < ActiveRecord::Migration[7.1]
       t.string :status
 
       t.timestamps
+
+      t.index: player_black_id
+      t.index: player_white_id
     end
   end
 end
