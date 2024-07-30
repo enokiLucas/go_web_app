c51899b3c7b4a9c479b9d03f1d35efbc85108566
Lucas Enoki <enoki.lucas@gmail.com>
2024-07-30 16:28:41 -0300

    Access constants in ruby code

---
+++ config/initializers/load_constants.rb
@@ -0,0 +1,12 @@
+CONSTANTS = JSON.parse(File.read(Rails.root.join('config/constants/constants.json')))
+
+=begin Example usage in a controller
+
+class GameController < ApplicationController
+  def show
+    edge_margin = CONSTANTS['EDGE_MARGIN']
+    render plain: "Edge Margin is #{edge_margin}"
+  end
+end
+
+=end
