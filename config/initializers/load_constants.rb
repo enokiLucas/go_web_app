ed0ba6102f2d214c3a9da9267e2f5914b1b823d2
Lucas Enoki <enoki.lucas@gmail.com>
2024-07-30 17:10:30 -0300

    fixed path

--- config/initializers/load_constants.rb
+++ config/initializers/load_constants.rb
@@ -1,10 +1,10 @@
-CONSTANTS = JSON.parse(File.read(Rails.root.join('app/config/constants/constants.json')))
+CONSTANTS = JSON.parse(File.read(Rails.root.join('	config/constants/constants.json')))
 
 =begin Example usage in a controller
 
 class GameController < ApplicationController
   def show
-    edge_margin = CONSTANTS['EDGE_MARGIN']
+    edge_margin = CONSTANTS['EDGE_MARGIN']	
     render plain: "Edge Margin is #{edge_margin}"
   end
 end
