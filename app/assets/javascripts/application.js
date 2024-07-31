43b8dab9b9e94ec825002dcbf8d6eb3d2fdcd5f5
Lucas Enoki <enoki.lucas@gmail.com>
2024-07-31 15:22:14 -0300

    embed constants into the view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,13 +1,1 @@
-/**
- * Test
- * console.log('Hello from assets!'); can talk to the console.
- */
-
-import { loadConstants } from './utils/constants';
-
-document.addEventListener('turbo:load', () => {
-  loadConstants((constants) => {
-    console.log('Constants loaded:', constants);  // Add this line for debugging
-
-  });
-});
+console.log(window.CONSTANTS.OFFSET);  // Outputs: value1
