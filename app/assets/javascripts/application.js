1c90b0acbb1e2fb96dd17f185acaccff3a7f9f71
Lucas Enoki <enoki.lucas@gmail.com>
2024-08-01 08:52:31 -0300

    added 'require_tree' for the utils and services directories

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,7 @@
 // Include all JavaScript files in the components directory
 //= require_tree ./components
+//= require_tree ./utils
+//= require_tree ./services
 
 document.addEventListener('turbo:load', () => {
   // Initialize custom elements
