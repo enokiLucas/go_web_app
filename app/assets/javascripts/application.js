0ceb527d026c910b542b2c7f5290497ec9379082
Lucas Enoki <enoki.lucas@gmail.com>
2024-08-02 12:20:23 -0300

    commented out element initialization

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,12 +2,12 @@
 //= require_tree ./components
 //= require_tree ./utils
 //= require_tree ./services
-
+/*
 document.addEventListener('turbo:load', () => {
   // Initialize custom elements
   customElements.define('go-board', GoBoard);
   // ... initialize other custom elements
 });
-
+*/
 //Test the constants
 console.log(window.CONSTANTS.OFFSET);  // Outputs: value1
